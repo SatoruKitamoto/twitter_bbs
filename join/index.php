d8b03fb222fd7cc524552c87bbaa9913006d1d50
satorukitamoto <otankonasu220@gmail.com>
2015-11-04 19:37:37 +0800

    宿題 email,password 未入力時エラーメッセージ

--- join/index.php
+++ join/index.php
@@ -37,13 +37,31 @@ if(!empty($_POST)){
 	if(strlen($_POST['password']) < 4 ){
 		$error['password'] = 'length';
 	}
-	if($_POST['password'] =''){
+	
+
+
+	if($_POST['password'] ==''){
 		$error['password'] = 'blank';
 	}
+	$filename = $_FILES['image']['name'];
+	if (!empty($fileName)){
+		$ext = substr($fileName, -3);
+		if ($ext != 'jpg' && $ext != 'gif'){
+			$error['image'] = 'type';
+		}
+	}
+	// var_dump($error);
+	// var_dump();
+
 
 //正常に入力されていたら
 	if(empty($error)){
+		//画像をアップロードする
+		$image = date('YmdHis') . $_FILES['image']['name'];
+		move_uploaded_file($_FILES['image']['tmp_name'], '../member_picture/' . $image);
+
 		$_SESSION['join'] = $_POST;
+		$_SESSION['join']['image'] = $image;
 		//画面遷移
 		header('Location: check.php');
  		exit();
@@ -78,11 +96,28 @@ if(!empty($_POST)){
 		</dd>
 		<dt>メールアドレス<span class="required">必須</span></dt>
 		<dd>
-			<input type="text" name="email" size="35" maxlength="255" />
+			<input type="text" name="email" size="35" maxlength="255" value="<?php echo htmlspecialchars($_POST['email'], ENT_QUOTES, 'UTF-8'); ?>" />
+			<?php 
+			if(isset($error['email'])&&($error['email'] == 'blank')): 
+				?>
+			<p class="error">*メールアドレスを入力して下さい</p>
+			<?php endif; ?>
+
+
 		</dd>
 		<dt>パスワード<span class="required">必須</span></dt>
 		<dd>
-			<input type="password" name="password" size="10" maxlength="20" />
+			<input type="password" name="password" size="10" maxlength="20" value="<?php echo htmlspecialchars($_POST['password'], ENT_QUOTES, 'UTF-8'); ?>" />
+			<?php if(isset($error['password'])&&($error['password'] == 'blank')): ?>
+			<p class="error">*パスワードを入力して下さい</p>
+			<?php 
+			endif;
+			 ?>
+			<?php if ($error['password'] == 'length'); ?>
+			<p class="error">*パスワードは4文字以上で入力して下さい</p>
+			<?php 
+			// endif; 
+			?>	
 		</dd>
 		<dt>写真など</dt>
 		<dd>
